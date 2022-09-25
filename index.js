a687cc0ffef23b1de4b14e1e4088f36e8d0d0a0d
Kiana Long <Kianajade42.kl@gmail.com>
2022-09-25 11:44:48 -0400

    css animation update

--- index.js
+++ index.js
@@ -1,52 +1,75 @@
+// function getUsersData () {
+//     return fetch('http://localhost:3000/users')
+//     .then(res => res.json())
+//     .then(data => {
+//         allUsers = data
+//     })
+// }
+const URL = 'http://localhost:3000/users'
 
-function ready() {
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new Match(150, cards);
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipcard(card)
-        });
-    });
-}
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMcontentLoaded', ready());
-} else {
-    ready();
-}
+const player = {
 
-class Match {
-    constructor(time, cards) {
-        this.time = time;
-        this.cards = cards;
-        this.timeRemaining = time;
-        this.timer = document.getElementById('timer');
-    }
-    startCountDown() {
-        return setInterval(() => {
-            this.time--;
-            this.time.innerText = this.timeRemaining;
-            if (this.timeRemaining === 0)
-                this.stop();
-        }, 150);
-    }
-    startGame() {
-        this.cardValid = null;
-        this.totalClicks = 0;
-        this.time = this.time;
-        this.matches = [];
-        this.busy = true;
-        this.shuffleCards();
-    }
-    validFlipcard(card) {
-        return true;
-        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardValid;
-    }
-    flipCard(card) {
-        if (this.canFlipCard(card)) {
-            card.classList.add('visible')
-        }
+  getUsers: () => {
+    return fetch(`${URL}/users`)
+    .then(res=>res.json())
+  },
 
-    }
+  createUser: (username) => {
+    return fetch(`${URL}/users`, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(username)
+    })
+    .then(res => res.json())
+  },
+// function ready() {
+//     let cards = Array.from(document.getElementsByClassName('card'));
+//     let game = new Match(150, cards);
+//     cards.forEach(card => {
+//         card.addEventListener('click', () => {
+//             game.flipcard(card)
+//         });
+//     });
+// }
+// if (document.readyState === 'loading') {
+//     document.addEventListener('DOMcontentLoaded', ready());
+// } else {
+//     ready();
+// }
+
+// class Match {
+//     constructor(time, cards) {
+//         this.time = time;
+//         this.cards = cards;
+//         this.timeRemaining = time;
+//         this.timer = document.getElementById('timer');
+//     }
+//     startCountDown() {
+//         return setInterval(() => {
+//             this.time--;
+//             this.time.innerText = this.timeRemaining;
+//             if (this.timeRemaining === 0)
+//                 this.stop();
+//         }, 150);
+//     }
+//     startGame() {
+//         this.cardValid = null;
+//         this.totalClicks = 0;
+//         this.time = this.time;
+//         this.matches = [];
+//         this.busy = true;
+//         this.shuffleCards();
+//     }
+//     validFlipcard(card) {
+//         return true;
+//         //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardValid;
+//     }
+//     flipCard(card) {
+//         if (this.canFlipCard(card)) {
+//             card.classList.add('visible')
+//         }
+
+//     }
     //  shuffleCards() {
     // for (let i = this.cardsArray.length - 1; i > 0; i--) (
     //   let shuffle = Math.floor(Math.random() * (i + 1));
@@ -54,4 +77,4 @@ class Match {
     //  this.cardsArray[i].style.order = shuffle;
     // )
 
-}
+// }
